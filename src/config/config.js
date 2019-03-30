8bea7705ab3e520377acacbbb0d64e19c516a7de
kangyonggan <keeo0qkz>
2019-03-30 11:49:46 +0800

    lockScreen

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ import Env from './env';
 
 let config = {
     env: Env,
-    baseUrl: Env === 'development' ? 'http://localhost:8888' : 'https://kangyonggan.com'
+    baseUrl: Env === 'development' ? 'http://localhost:8080' : 'https://kangyonggan.com'
 };
 
 export default config;
